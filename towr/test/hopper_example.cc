954f7a5fc1bb82e4e529504d0c8ce80550cc51b4
alexyskoutnev <alexyskoutnev@gmail.com>
2023-04-08 14:35:48 -0500

    input/output interface

--- towr/test/hopper_example.cc
+++ towr/test/hopper_example.cc
@@ -46,66 +46,84 @@ using namespace towr;
 
 
 void entry(std::ofstream& file, double joints[]){
-    for (int i = 0; i < 11; i++){
+    for (int i = 0; i < 14; i++){
       file << joints[i] << ",";
     }
-    file << joints[11] << "\n";
+    file << joints[15] << "\n";
 }
 
 void getTrajectory(SplineHolder& solution, std::string save_file, double timestep){
   double t = 0.0;
   double T = solution.base_linear_->GetTotalTime();
-  double joints[12];
+  double csv[15];
   std::ofstream file;
   file.open(save_file);
   Eigen::VectorXd base_lin, base_ang, base_ang_vel, base_ang_acc, ee_contact, ee_motion, ee_forces;
   while (t <= T + 1e-4){
     int n_ee = solution.ee_motion_.size();
+    base_lin = solution.base_linear_->GetPoint(t).p();
+    for (int i = 0; i < 3; i++){
+        csv[i] = base_lin[i];
+    }
     for (int ee_towr=0; ee_towr<n_ee; ++ee_towr) {
         ee_motion  = solution.ee_motion_.at(ee_towr)->GetPoint(t).p();
         // std::cout << "EE-POS [" << ee_towr << "] " << std::endl;
         // std::cout << ee_motion << std::endl;
         for (int i = 0; i < 3; i++){
-            joints[(ee_towr * 3) + i] = ee_motion[i];
+            csv[(ee_towr * 3) + (i + 3)] = ee_motion[i];
         }
     }
-    entry(file, joints);
+    entry(file, csv);
     t += timestep;
   }
   file.close();
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
   NlpFormulation formulation;
+  double goal[3];
+  double start[3];
+  //start-end position
+  if (argc > 1){
+    std::cout << "Using CMD VALUES" << std::endl;
+    for (int i = 1; i < argc; i++){
+      goal[i - 1] = std::stod(argv[i]);
+    }
+  } else {
+      std::cout << "Default" << std::endl;
+      goal[0] = 1.0;
+      goal[1] = 0.0;
+      goal[2] = 0.317;
+  }
 
   //timetep 
-  double timestep = 0.001;
+  double timestep = 0.01;
 
   //number of EE 
   int n_ee = 4;
 
   //trajectory run time
-  double run_time = 5.0;
+  double run_time = 10.0;
 
   // terrain
   formulation.terrain_ = std::make_shared<FlatGround>(0.0);
 
   // Kinematic limits and dynamic parameters of the Quadruped
   // formulation.model_ = RobotModel(RobotModel::Biped);
-  // formulation.model_ = RobotModel(RobotModel::Solo12);
-  formulation.model_ = RobotModel(RobotModel::Anymal);
+  formulation.model_ = RobotModel(RobotModel::Solo12);
+  // formulation.model_ = RobotModel(RobotModel::Anymal);
 
   //Set the initial EE position at t = 0
   auto nominal_stance_B = formulation.model_.kinematic_model_->GetNominalStanceInBase();
   formulation.initial_ee_W_ = nominal_stance_B;
 
   // set the initial position of the quadruped
-  formulation.initial_base_.lin.at(kPos).z() = 0.5;
+  formulation.initial_base_.lin.at(kPos).z() = 0.317;
   
   // define the desired goal state of the quadruped
-  formulation.final_base_.lin.at(towr::kPos) << 1.0, 0.0, 0.5;
+  formulation.final_base_.lin.at(towr::kPos) << goal[0], goal[1], goal[2];
 
   // Parameters that define the motion. See c'tor for default values or
   // other values that can be modified.
@@ -113,7 +131,7 @@ int main()
   // by the optimizer. The number of swing and stance phases however is fixed.
   // alternating stance and swing:     ____-----_____-----_____-----_____
   auto gait_gen_ = GaitGenerator::MakeGaitGenerator(n_ee);
-  auto id_gait   = static_cast<GaitGenerator::Combos>(0);
+  auto id_gait   = static_cast<GaitGenerator::Combos>(1);
     gait_gen_->SetCombo(id_gait);
     for (int ee=0; ee<n_ee; ++ee) {
       formulation.params_.ee_phase_durations_.push_back(gait_gen_->GetPhaseDurations(run_time, ee));
@@ -139,7 +157,7 @@ int main()
   // solver->SetOption("derivative_test", "first-order");
   auto solver = std::make_shared<ifopt::IpoptSolver>();
   solver->SetOption("jacobian_approximation", "exact"); // "finite difference-values"
-  solver->SetOption("max_cpu_time", 50.0);
+  solver->SetOption("max_cpu_time", 30.0);
   solver->Solve(nlp);
 
   // Can directly view the optimization variables through:
