e1c626134479c185ba8ce1eb463ac768455af3e2
alexyskoutnev <alexyskoutnev@gmail.com>
2023-04-28 18:38:43 +0000

    added more parameters to towr script

--- towr/test/hopper_example.cc
+++ towr/test/hopper_example.cc
@@ -16,6 +16,20 @@ std::string save_file = "traj.csv";
 
 using namespace towr;
 
+std::vector<std::string> split(const std::string& str, char delimiter) {
+    std::vector<std::string> substrings;
+    size_t start = 0;
+    size_t end = str.find(delimiter);
+    while (end != std::string::npos) {
+        substrings.push_back(str.substr(start, end - start));
+        start = end + 1;
+        end = str.find(delimiter, start);
+    }
+    substrings.push_back(str.substr(start));
+    return substrings;
+}
+
+
 std::string n_split_tokens(char** begin, int cnt){
   std::string input = "";
   int i = 0;
@@ -39,11 +53,15 @@ std::string getcmdParser(char** begin, char** end, const std::string & option, i
   if (itr != end && ++itr != end){
     return n_split_tokens(itr, cnt);
   }
-  return 0;
+  return "";
 }
 
 bool cmdoptionExists(char** begin, char** end, const std::string& option){
-  return std::find(begin, end, option);
+  char** itr = std::find(begin, end, option);
+  if (itr != end && ++itr != end){
+    return 1;
+  }
+  return 0;
 }
 
 void entry(std::ofstream& file, double joints[]){
@@ -89,47 +107,85 @@ int main(int argc, char* argv[])
   NlpFormulation formulation;
   double goal[3];
   double start[3];
+  double start_vel[3];
+  double start_ang[3];
+  double start_orn_vel[3];
+  double goal_vel[3];
 
-  try {
-  if (cmdoptionExists(argv, argv+argc, "-g"))
-    {
-      std::string cmd_return = getcmdParser(argv, argv+argc, "-g", 3);
-      // goal[0] = std::stod(cmd_return[0]);
-      // goal[1] = std::stod(cmd_return[1]);
-      // goal[2] = std::stod(cmd_return[2]);
-    }
-  else
-    {
-      goal[0] = 0.5;
-      goal[1] = 0.0;
-      goal[2] = 0.21;
-    }
-  }
-  catch (const std::invalid_argument& e){
-    std::cerr << "Argument input error" << std::endl;
-    std::cerr << "Error: " << e.what() << std::endl;
-  }
 
-  //start-end position
-  // if (argc > 1){
-  //   std::cout << "Using CMD VALUES" << std::endl;
-  //   for (int i = 1; i < argc - 1; i++){
-  //     if (i > 0 && i < 4): //Recording start COM
-  //     {
-  //         start[i - 1] = std::stod(argv[i]);
-  //     } else if (i > 3 && i < 6){
-  //         goal[i - 1] = std::stod(argv[i + 1]);
-  //     }
-  //   }
-  // } else {
-  //     std::cout << "Default" << std::endl;
-  //     start[0] = 0.0;
-  //     start[1] = 0.0;
-  //     start[2] = 0.21;
-      // goal[0] = 0.5;
-      // goal[1] = 0.0;
-      // goal[2] = 0.21;
-  // }
+  if (argc > 1){
+    try {
+    if (cmdoptionExists(argv, argv+argc, "-g"))
+      {
+        std::vector<std::string> cmd_return = split(getcmdParser(argv, argv+argc, "-g", 3), ' ');
+        goal[0] = std::stod(cmd_return[0]);
+        goal[1] = std::stod(cmd_return[1]);
+        goal[2] = std::stod(cmd_return[2]);
+      }
+    else
+      {
+        goal[0] = 0.5;
+        goal[1] = 0.0;
+        goal[2] = 0.21;
+      }
+    if (cmdoptionExists(argv, argv+argc, "-s"))
+      {
+        std::vector<std::string> cmd_return = split(getcmdParser(argv, argv+argc, "-s", 3), ' ');
+        start[0] = std::stod(cmd_return[0]);
+        start[1] = std::stod(cmd_return[1]);
+        start[2] = std::stod(cmd_return[2]);
+      }
+    else
+      {
+        start[0] = 0.0;
+        start[1] = 0.0;
+        start[2] = 0.21;
+      }
+    if (cmdoptionExists(argv, argv+argc, "-s_ang"))
+      {
+        std::vector<std::string> cmd_return = split(getcmdParser(argv, argv+argc, "-s_ang", 3), ' ');
+        start_ang[0] = std::stod(cmd_return[0]);
+        start_ang[1] = std::stod(cmd_return[1]);
+        start_ang[2] = std::stod(cmd_return[2]);
+      }
+    else
+      {
+        start_ang[0] = 0.0;
+        start_ang[1] = 0.0;
+        start_ang[2] = 0.0;
+      }
+    if (cmdoptionExists(argv, argv+argc, "-s_vel"))
+      {
+        std::vector<std::string> cmd_return = split(getcmdParser(argv, argv+argc, "-s_vel", 3), ' ');
+        start_vel[0] = std::stod(cmd_return[0]);
+        start_vel[1] = std::stod(cmd_return[1]);
+        start_vel[2] = std::stod(cmd_return[2]);
+      }
+    else
+      {
+        start_vel[0] = 0.0;
+        start_vel[1] = 0.0;
+        start_vel[2] = 0.0;
+      }
+    }
+    catch (const std::invalid_argument& e){
+      std::cerr << "Argument input error" << std::endl;
+      std::cerr << "Error: " << e.what() << std::endl;
+    }
+  } else {
+    start[0] = 0.0;
+    start[1] = 0.0;
+    start[2] = 0.21;
+    start_vel[0] = 0.0;
+    start_vel[1] = 0.0;
+    start_vel[2] = 0.0;
+    start_ang[0] = 0.0;
+    start_ang[1] = 0.0;
+    start_ang[2] = 0.0;
+    goal[0] = 0.5;
+    goal[1] = 0.0;
+    goal[2] = 0.21;
+  }
 
   //start of ground
   double z_ground = 0.0;
@@ -162,10 +218,9 @@ int main(int argc, char* argv[])
   // set the initial position of the quadruped
   formulation.initial_base_.lin.at(kPos).z() = -nominal_stance_B.front().z() + z_ground;
   goal[2] = formulation.initial_base_.lin.at(kPos).z();
+  // start[2] = formulation.initial_base_.lin.at(kPos).z();
 
-  std::cout << "base start pos x -> " << formulation.initial_base_.lin.at(kPos).x() << std::endl;
-  std::cout << "base start pos y -> " << formulation.initial_base_.lin.at(kPos).y() << std::endl;
-  std::cout << "base start pos z -> " << formulation.initial_base_.lin.at(kPos).z() << std::endl;
+ 
 
   // Starting the EE position a height zero
   for (auto ee : formulation.initial_ee_W_){
@@ -176,15 +231,36 @@ int main(int argc, char* argv[])
   }
   
 
-  //define the start location of quadruped
-  formulation.initial_base_.lin.at(kPos).x() = start[0];
-  formulation.initial_base_.lin.at(kPos).y() = start[1];
+  //define the start conditions for quadruped
+  formulation.initial_base_.lin.at(towr::kPos).x() = start[0];
+  formulation.initial_base_.lin.at(towr::kPos).y() = start[1];
+  formulation.initial_base_.lin.at(towr::kPos).z() = start[2];
+  formulation.initial_base_.lin.at(towr::kVel).x() = start_vel[0];
+  formulation.initial_base_.lin.at(towr::kVel).y() = start_vel[1];
+  formulation.initial_base_.lin.at(towr::kVel).z() = start_vel[2];
+  formulation.initial_base_.ang.at(towr::kPos).x() = start_ang[0];
+  formulation.initial_base_.ang.at(towr::kPos).y() = start_ang[1];
+  formulation.initial_base_.ang.at(towr::kPos).z() = start_ang[2];
 
   // define the desired goal state of the quadruped
   formulation.final_base_.lin.at(towr::kPos) << goal[0], goal[1], goal[2];
   formulation.final_base_.lin.at(towr::kVel) << 0, 0, 0;
   formulation.final_base_.ang.at(towr::kPos) << 0, 0, 0;
-  formulation.final_base_.ang.at(towr::kVel) << 0.5, 0, 0;
+  formulation.final_base_.ang.at(towr::kVel) << 0, 0, 0;
+
+  std::cout << "base start pos x -> " << formulation.initial_base_.lin.at(kPos).x() << std::endl;
+  std::cout << "base start pos y -> " << formulation.initial_base_.lin.at(kPos).y() << std::endl;
+  std::cout << "base start pos z -> " << formulation.initial_base_.lin.at(kPos).z() << std::endl;
+  std::cout << "base start vel x -> " << formulation.initial_base_.lin.at(kVel).x() << std::endl;
+  std::cout << "base start vel y -> " << formulation.initial_base_.lin.at(kVel).y() << std::endl;
+  std::cout << "base start vel z -> " << formulation.initial_base_.lin.at(kVel).z() << std::endl;
+  std::cout << "base start ang x -> " << formulation.initial_base_.ang.at(kPos).x() << std::endl;
+  std::cout << "base start ang y -> " << formulation.initial_base_.ang.at(kPos).y() << std::endl;
+  std::cout << "base start ang z -> " << formulation.initial_base_.ang.at(kPos).z() << std::endl;
+
+  std::cout << "base end pos x -> " << formulation.final_base_.lin.at(kPos).x() << std::endl;
+  std::cout << "base end pos y -> " << formulation.final_base_.lin.at(kPos).y() << std::endl;
+  std::cout << "base end pos z -> " << formulation.final_base_.lin.at(kPos).z() << std::endl;
 
 
   //auto gait generation
@@ -197,7 +273,6 @@ int main(int argc, char* argv[])
   }
 
   formulation.params_.constraints_.push_back(Parameters::BaseRom); //restricts the basemotion (adds more desicion varaibles  nd helps optumization converge)
-  // formulation.params_.OptimizePhaseDurations();
 
   // Initialize the nonlinear-programming problem with the variables,
   // constraints and costs.
@@ -217,7 +292,7 @@ int main(int argc, char* argv[])
   solver->SetOption("jacobian_approximation", "exact"); // "finite difference-values"
   solver->SetOption("max_cpu_time", 60.0);
   solver->SetOption("print_level", 5);
-  solver->SetOption("max_iter", 3000);
+  solver->SetOption("max_iter", 1000);
   solver->Solve(nlp);
   using namespace std;
   cout.precision(2);
