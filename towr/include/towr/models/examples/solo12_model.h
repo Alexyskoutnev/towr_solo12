c8cd534da74d6f301172d8a90dce628fe442435d
cinaral <cinar.al@outlook.com>
2023-08-01 11:20:07 -0500

    Remove deadcode, .vscode files Add .gitignore Add comments for defaults

--- towr/include/towr/models/examples/solo12_model.h
+++ towr/include/towr/models/examples/solo12_model.h
@@ -14,14 +14,6 @@ class Solo12KinematicModel : public KinematicModel {
 public:
   Solo12KinematicModel () : KinematicModel(4)
   {
-    // const double x_nominal_b = 0.21;
-    // // const double x_nominal_b = 0.21;
-    // const double x_nominal_b = 0.25;
-    // // const double y_nominal_b = 0.15;
-    // const double y_nominal_b = 0.25;
-    // const double z_nominal_b = -0.25;
-    // const double z_nominal_b = 0.0;
-
     const double x_nominal_b = 0.20;
     const double y_nominal_b = 0.18;
     const double z_nominal_b = -0.24;
@@ -31,10 +23,7 @@ public:
     nominal_stance_.at(LH) << -x_nominal_b,   y_nominal_b, z_nominal_b;
     nominal_stance_.at(RH) << -x_nominal_b,  -y_nominal_b, z_nominal_b;
 
-    // max_dev_from_nominal_ << 0.10, 0.10, 0.10;
-    // max_dev_from_nominal_ << 0.10, 0.10, 0.05;
     max_dev_from_nominal_ << 0.10, 0.08, 0.075;
-    // max_dev_from_nominal_ << 0.15, 0.15, 0.10;
   }
 };
 
--- towr_ros/src/towr_user_interface.cc
+++ towr_ros/src/towr_user_interface.cc
@@ -64,13 +64,13 @@ TowrUserInterface::TowrUserInterface ()
   user_command_pub_ = n.advertise<towr_ros::TowrCommand>(towr_msgs::user_command, 1);
 
   goal_geom_.lin.p_.setZero();
-  goal_geom_.lin.p_ << 0.5, 0.0, 0.0;
+  goal_geom_.lin.p_ << 0.5, 0.0, 0.0; // default: 2.1, 0.0, 0.0
   goal_geom_.ang.p_ << 0.0, 0.0, 0.0; // roll, pitch, yaw angle applied Z->Y'->X''
 
   robot_      = RobotModel::Solo12;
   terrain_    = HeightMap::FlatID;
   gait_combo_ = GaitGenerator::C0;
-  total_duration_ = 5; //2.4
+  total_duration_ = 5; // default: 2.4
   visualize_trajectory_ = false;
   plot_trajectory_ = false;
   replay_speed_ = 1.0; // realtime
