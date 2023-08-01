408d2dcabf32f3545d6e4a32fbcc8ea8a2576710
cinaral <cinar.al@outlook.com>
2023-08-01 15:54:07 -0500

    Add two boxes as custom terrain

--- towr/include/towr/terrain/custom_terrain.hpp
+++ towr/include/towr/terrain/custom_terrain.hpp
@@ -18,22 +18,17 @@ public:
   double GetHeight(double x, double y) const override;
 
 private:
-  double first_step_start_  = 1.0;
-  double first_step_width_  = 0.4;
-  double height_first_step  = 0.2;
-  double height_second_step = 0.4;
-  double width_top = 1.0;
+  const double box1_x_pos_ = 0.5;  // [m] x position of the obstacle
+  const double box1_y_pos_ = -0.5; // [m] y position of the obstacle
+  const double box1_length_ = 0.5; // [m] length of the obstacle (in x direction)
+  const double box1_width_ = 1.0;  // [m] width of the obstacle (in y direction)
+  const double box1_height_ = 0.1; // [m] height of the obstacle
 
-  //const double box1_x_pos_ = 0.7;  // [m] x position of the obstacle
-  //const double box1_y_pos_ = -0.1; // [m] y position of the obstacle
-  //const double box1_length_ = 3.5;  // [m] length of the obstacle (in x direction)
-  //const double box1_width_ = .2;   // [m] width of the obstacle (in y direction)
-  //const double box1_height_ = .1; // [m] height of the obstacle
-  //const double box2_x_pos_ = 0.4;  // [m] x position of the obstacle
-  //const double box2_y_pos_ = 0.1;  // [m] y position of the obstacle
-  //const double box2_length_ = .2; // [m] length of the obstacle (in x direction)
-  //const double box2_width_ = .2;  // [m] width of the obstacle (in y direction)
-  //const double box2_height_ = .1; // [m] height of the obstacle
+  const double box2_x_pos_ = 1.7;  // [m] x position of the obstacle
+  const double box2_y_pos_ = -1.0; // [m] y position of the obstacle
+  const double box2_length_ = box1_length_; // [m] length of the obstacle (in x direction)
+  const double box2_width_ = box1_width_;  // [m] width of the obstacle (in y direction)
+  const double box2_height_ = 0.2; // [m] height of the obstacle
 };
 } // namespace towr
 
--- towr/src/custom_terrain.cpp
+++ towr/src/custom_terrain.cpp
@@ -5,15 +5,14 @@ namespace towr {
 double CustomTerrain::GetHeight(double x, double y) const {
   double h = 0.0;
 
-  if (x>=first_step_start_)
-    h = height_first_step;
+  if (x > box1_x_pos_ && x <= box1_x_pos_ + box1_length_ && y > box1_y_pos_ && y <= box1_y_pos_ + box1_width_) {
+    h = box1_height_;
+  }
 
-  if (x>=first_step_start_+first_step_width_)
-    h = height_second_step;
-
-  if (x>=first_step_start_+first_step_width_+width_top)
-    h = 0.0;
-
+  if (x > box2_x_pos_ && x <= box2_x_pos_ + box2_length_ && y > box2_y_pos_ && y <= box2_y_pos_ + box2_width_) {
+    h = box2_height_;
+  }
+ 
   return h;
 }
 
