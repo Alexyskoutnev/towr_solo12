bf8f59c3c3fc501ac8dae4477ad554933f5669e8
alexyskoutnev <alexyskoutnev@gmail.com>
2023-08-15 15:54:14 +0000

    build faster -j flags now

--- scripts/cmake_build/build.sh
+++ scripts/cmake_build/build.sh
@@ -16,6 +16,6 @@ TOWR_PATH=../../towr
 
 mkdir $TOWR_PATH/build
 pushdq $TOWR_PATH/build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -j4
 make
 echo "$0 returned $?."
